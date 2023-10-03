d75b7db332afaca760e0e0803af15657986694db
Chru Brar <chrubrar@gmail.com>
2023-10-02 22:58:42 -0400

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center px-4 py-4 bg-slate-500 text-zinc-900">
       <h1>Welcome to the Cloud Factory</h1>
-      <h3>This is a portfolio project for Charnjiv Brar</h3>
+      <div>
+        <h3>This is a portfolio project for Charnjiv Brar</h3>
+      </div>
     </div>
   );
 }
