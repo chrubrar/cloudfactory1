ec83a5df675ed9436b0346c771687d2daea95c32
Chru Brar <chrubrar@gmail.com>
2023-10-03 23:37:32 -0400

    pushing new div structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center px-4 py-4 bg-slate-500 text-zinc-900">
-      <h1>Welcome to the Cloud Factory</h1>
-      <div>
-        <h3>This is a portfolio project for Charnjiv Brar</h3>
-      </div>
-    </div>
-  );
+  return <div>Home Page</div>;
 }
